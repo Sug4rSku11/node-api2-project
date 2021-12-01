d2d6ecf2ea8c943722d85acd44818dcc83bbb044
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-11-30 17:50:05 -0700

    completed delete and get by id, test passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -87,8 +87,38 @@ router.put('/:id', (req, res) => {
 	}
 })
 //DELETE BY ID
+router.delete('/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const post = await Posts.findById(id)
+		if (!post) {
+			res.status(404).json({ message: "The post with the specified ID does not exist" })
+		} else {
+			await Posts.remove(id)
+			res.status(200).json(post)
+		}
+	} catch (error){
+		console.log(error)
+		res.status(500).json({ message: "The post could not be removed"})
+	}
+})
 
-//GET COMMENTS
+//GET COMMENTS BY ID
+router.get('/:id/comments', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const post = await Posts.findById(id)
+		if (!post) {
+			res.status(404).json({ message: "The post with the specified ID does not exist" })
+		} else {
+			const comments = await Posts.findPostComments(id)
+			res.status(200).json(comments)
+		}
+	} catch (error) {
+		console.error()
+		res.status(500).json({ message: "The comments information could not be retrieved" })
+	}
+})
 
 
 module.exports = router
